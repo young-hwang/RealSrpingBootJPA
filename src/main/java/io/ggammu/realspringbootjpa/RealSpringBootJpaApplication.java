3eb609b9b5fdc304333598f6af98067ea3becaf4
Young Hwang <ykcul02@gmail.com>
2021-11-06 22:33:52 +0900

    feat: Srping Boot JPA 애플린케이션 생성

---
+++ src/main/java/io/ggammu/realspringbootjpa/RealSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package io.ggammu.realspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealSpringBootJpaApplication.class, args);
+	}
+
+}
