94dba2eebd463dc48b8cdcb03211eaf4e349d616
Young Hwang <ykcul02@gmail.com>
2021-11-16 22:37:58 +0900

    feat: Member Service 추가

---
+++ src/main/java/io/ggammu/realspringbootjpa/service/MemberService.java
@@ -0,0 +1,35 @@
+package io.ggammu.realspringbootjpa.service;
+
+import io.ggammu.realspringbootjpa.domain.Member;
+import io.ggammu.realspringbootjpa.repository.MemberRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    // 회원 가입
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findAll() {
+        List<Member> findMembers = memberRepository.findAll();
+        return findMembers;
+    }
+
+}
