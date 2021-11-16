a019b4ee02b71d92c4945a6d3f78dbeb79f9f388
Young Hwang <ykcul02@gmail.com>
2021-11-16 22:46:45 +0900

    fix: repository setter injection으로 변환
    repository 주입이 자유롭지 않아 테스트 시 어려움 해결

--- src/main/java/io/ggammu/realspringbootjpa/service/MemberService.java
+++ src/main/java/io/ggammu/realspringbootjpa/service/MemberService.java
@@ -5,14 +5,21 @@ import io.ggammu.realspringbootjpa.repository.MemberRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class MemberService {
 
-    @Autowired
     private MemberRepository memberRepository;
 
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     // 회원 가입
+    @Transactional
     public Long join(Member member) {
         validateDuplicateMember(member);
         memberRepository.save(member);
@@ -27,9 +34,12 @@ public class MemberService {
     }
 
     // 회원 전체 조회
-    public List<Member> findAll() {
+    public List<Member> findMembers() {
         List<Member> findMembers = memberRepository.findAll();
         return findMembers;
     }
 
+    public Member findMember(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
 }
