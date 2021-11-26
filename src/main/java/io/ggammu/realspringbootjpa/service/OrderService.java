b8d2413bd9458cbe99b70514228ac9cfaf4006f4
Young Hwang <ykcul02@gmail.com>
2021-11-26 23:08:17 +0900

    feat: order service 주문 기능 추가

--- src/main/java/io/ggammu/realspringbootjpa/service/OrderService.java
+++ src/main/java/io/ggammu/realspringbootjpa/service/OrderService.java
@@ -1,6 +1,9 @@
 package io.ggammu.realspringbootjpa.service;
 
+import io.ggammu.realspringbootjpa.domain.Delivery;
 import io.ggammu.realspringbootjpa.domain.Member;
+import io.ggammu.realspringbootjpa.domain.Order;
+import io.ggammu.realspringbootjpa.domain.OrderItem;
 import io.ggammu.realspringbootjpa.domain.item.Item;
 import io.ggammu.realspringbootjpa.repository.ItemRepository;
 import io.ggammu.realspringbootjpa.repository.MemberRepository;
@@ -21,15 +24,23 @@ public class OrderService {
     private final ItemRepository itemRepository;
 
     //주문
+    @Transactional
     public Long order(Long memberId, Long itemId, int count) {
         //엔티티 조회
         Member member = memberRepository.findOne(memberId);
         Item item = itemRepository.findOne(itemId);
 
         //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
 
         //주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
 
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+        orderRepository.save(order);
+        return order.getId();
     }
 
     //취소
