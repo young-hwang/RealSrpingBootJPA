955a75b27c9707a6a73357b5248ad3bb676b683d
Young Hwang <ykcul02@gmail.com>
2021-11-30 22:45:41 +0900

    feat: jpql을 이용한 복잡한 조회

--- src/main/java/io/ggammu/realspringbootjpa/repository/OrderRepository.java
+++ src/main/java/io/ggammu/realspringbootjpa/repository/OrderRepository.java
@@ -3,8 +3,11 @@ package io.ggammu.realspringbootjpa.repository;
 import io.ggammu.realspringbootjpa.domain.Order;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.NamedParameterBatchUpdateUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 @RequiredArgsConstructor
 @Repository
@@ -20,14 +23,38 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    public List<Order> findAll(OrderSearch orderSearch) {
-        return em.createQuery("select o from Order o join o.member m" +
-                        "where o.status = :status" +
-                        "and m.name like :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
-                .setMaxResults(1000)
-                .getResultList();
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += "where ";
+                isFirstCondition = false;
+            }
+            jpql += "o.status = :status";
+        }
+
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += "where ";
+            } else {
+                jpql += "and ";
+            }
+            jpql += "m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query.setParameter("status", orderSearch.getOrderStatus());
+        }
+
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
     }
 
 }
