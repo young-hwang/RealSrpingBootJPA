16f9d159dd993ceb6e3a4cca1baf9acd94c0fc6e
Young Hwang <ykcul02@gmail.com>
2021-12-08 22:43:44 +0900

    feat: Member 등록 서비스 추가

--- src/main/java/io/ggammu/realspringbootjpa/controller/MemberController.java
+++ src/main/java/io/ggammu/realspringbootjpa/controller/MemberController.java
@@ -1,10 +1,14 @@
 package io.ggammu.realspringbootjpa.controller;
 
+import io.ggammu.realspringbootjpa.domain.Address;
+import io.ggammu.realspringbootjpa.domain.Member;
 import io.ggammu.realspringbootjpa.service.MemberService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,4 +21,18 @@ public class MemberController {
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
     }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm memberForm) {
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
 }
