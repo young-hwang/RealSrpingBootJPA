498db6b4e818ed893b5c40c319fb4fc7c6c8be7d
Young Hwang <ykcul02@gmail.com>
2021-12-13 22:40:01 +0900

    feat: 제품 조회 화면

--- src/main/java/io/ggammu/realspringbootjpa/controller/ItemController.java
+++ src/main/java/io/ggammu/realspringbootjpa/controller/ItemController.java
@@ -33,4 +33,10 @@ public class ItemController {
         return "redirect:/";
     }
 
+    @GetMapping("/items")
+    public String list(Model model) {
+        model.addAttribute("items", itemService.findItems());
+        return "items/itemList";
+    }
+
 }
