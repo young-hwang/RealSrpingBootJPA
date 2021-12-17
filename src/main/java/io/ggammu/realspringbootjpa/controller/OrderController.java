05831888a79b602d72950ca5eb081e4aa2b15eab
Young Hwang <ykcul02@gmail.com>
2021-12-17 23:53:00 +0900

    feat: 주문 생성 기능 추가

---
+++ src/main/java/io/ggammu/realspringbootjpa/controller/OrderController.java
@@ -0,0 +1,50 @@
+package io.ggammu.realspringbootjpa.controller;
+
+import io.ggammu.realspringbootjpa.domain.Member;
+import io.ggammu.realspringbootjpa.domain.item.Item;
+import io.ggammu.realspringbootjpa.service.ItemService;
+import io.ggammu.realspringbootjpa.service.MemberService;
+import io.ggammu.realspringbootjpa.service.OrderService;
+import java.util.List;
+import javax.jws.WebParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orders(Model model) {
+        return "/order/orderList";
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into member (city, street, zipcode, name, member_id) values ('city', 'street', '111-111', 'name', 1);
+insert into item (name, price, stock_quantity, author, isbn, dtype, item_id) values ('item', 1, 1, 'author', '1234', 'B', 2);
