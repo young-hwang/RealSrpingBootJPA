3446f5df94156e9e705243ef253ef0674ecfed9a
Young Hwang <ykcul02@gmail.com>
2021-12-20 21:59:15 +0900

    feat: member 등록 api 생성

---
+++ src/main/java/io/ggammu/realspringbootjpa/api/MemberApiController.java
@@ -0,0 +1,34 @@
+package io.ggammu.realspringbootjpa.api;
+
+import io.ggammu.realspringbootjpa.domain.Member;
+import io.ggammu.realspringbootjpa.service.MemberService;
+import javax.validation.Valid;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMember(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+}
