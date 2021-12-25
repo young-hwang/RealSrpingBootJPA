db22f353efa76965f90b1fc1a65ae8dd34656383
Young Hwang <ykcul02@gmail.com>
2021-12-25 22:48:36 +0900

    feat: 엔티티를 노출하지 않는 API 구성

--- src/main/java/io/ggammu/realspringbootjpa/api/OrderApiController.java
+++ src/main/java/io/ggammu/realspringbootjpa/api/OrderApiController.java
@@ -1,10 +1,14 @@
 package io.ggammu.realspringbootjpa.api;
 
+import io.ggammu.realspringbootjpa.domain.Address;
 import io.ggammu.realspringbootjpa.domain.Order;
-import io.ggammu.realspringbootjpa.repository.OrderRepository;
+import io.ggammu.realspringbootjpa.domain.OrderStatus;
 import io.ggammu.realspringbootjpa.repository.OrderSearch;
 import io.ggammu.realspringbootjpa.service.OrderService;
+import java.time.LocalDateTime;
 import java.util.List;
+import static java.util.stream.Collectors.*;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +27,37 @@ public class OrderApiController {
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> orderV1() {
-        return orderService.findOrdersByString(new OrderSearch());
+        List<Order> all = orderService.findOrdersByString(new OrderSearch());
+        for (Order order: all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+        }
+        return all;
     }
 
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2() {
+        List<Order> orders= orderService.findOrdersByString(new OrderSearch());
+        List<SimpleOrderDto> simpleOrderDtos = orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(toList());
+        return simpleOrderDtos;
+    }
+
+    @Data
+    private class SimpleOrderDto {
+        private Long id;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            id = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
 }
