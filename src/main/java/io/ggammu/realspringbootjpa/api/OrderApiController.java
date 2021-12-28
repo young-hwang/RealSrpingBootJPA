9db388e7c802b8c5221e77cd84b4c69c50e5847d
Young Hwang <ykcul02@gmail.com>
2021-12-28 22:51:35 +0900

    feat: OneToMany 형태의 데이터 조인 api 반환

--- src/main/java/io/ggammu/realspringbootjpa/api/OrderApiController.java
+++ src/main/java/io/ggammu/realspringbootjpa/api/OrderApiController.java
@@ -1,76 +1,30 @@
 package io.ggammu.realspringbootjpa.api;
 
-import io.ggammu.realspringbootjpa.domain.Address;
 import io.ggammu.realspringbootjpa.domain.Order;
-import io.ggammu.realspringbootjpa.domain.OrderStatus;
+import io.ggammu.realspringbootjpa.domain.OrderItem;
+import io.ggammu.realspringbootjpa.repository.OrderRepository;
 import io.ggammu.realspringbootjpa.repository.OrderSearch;
-import io.ggammu.realspringbootjpa.repository.order.simplequery.OrderSimpleQueryDto;
-import io.ggammu.realspringbootjpa.service.OrderService;
-import java.time.LocalDateTime;
 import java.util.List;
-import static java.util.stream.Collectors.*;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * xToOne(ManyToOne, OneToOne)
- * Order
- * Order -> Member
- * Order -> Delivery
- */
 @RequiredArgsConstructor
 @RestController
 public class OrderApiController {
 
-    private final OrderService orderService;
+    private final OrderRepository orderRepository;
 
-    @GetMapping("/api/v1/simple-orders")
+    @GetMapping("/api/v1/orders")
     public List<Order> orderV1() {
-        List<Order> all = orderService.findOrdersByString(new OrderSearch());
-        for (Order order: all) {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
             order.getMember().getName();
             order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
         }
         return all;
     }
 
-    @GetMapping("/api/v2/simple-orders")
-    public List<SimpleOrderDto> orderV2() {
-        List<Order> orders= orderService.findOrdersByString(new OrderSearch());
-        List<SimpleOrderDto> simpleOrderDtos = orders.stream()
-                .map(SimpleOrderDto::new)
-                .collect(toList());
-        return simpleOrderDtos;
-    }
-
-    @GetMapping("/api/v3/simple-orders")
-    public List<SimpleOrderDto> orderV3() {
-        List<Order> orders = orderService.findAllWithMemberDelivery();
-        List<SimpleOrderDto> simpleOrderDtos = orders.stream().map(SimpleOrderDto::new).collect(toList());
-        return simpleOrderDtos;
-    }
-
-    @GetMapping("/api/v4/simple-orders")
-    public List<OrderSimpleQueryDto> orderV4() {
-        return orderService.findAllOrderDto();
-    }
-
-    @Data
-    private class SimpleOrderDto {
-        private Long id;
-        private String name;
-        private LocalDateTime orderDate;
-        private OrderStatus orderStatus;
-        private Address address;
-
-        public SimpleOrderDto(Order order) {
-            id = order.getId();
-            name = order.getMember().getName();
-            orderDate = order.getOrderDate();
-            orderStatus = order.getStatus();
-            address = order.getDelivery().getAddress();
-        }
-    }
 }
---
+++ src/main/java/io/ggammu/realspringbootjpa/api/OrderSimpleApiController.java
@@ -0,0 +1,76 @@
+package io.ggammu.realspringbootjpa.api;
+
+import io.ggammu.realspringbootjpa.domain.Address;
+import io.ggammu.realspringbootjpa.domain.Order;
+import io.ggammu.realspringbootjpa.domain.OrderStatus;
+import io.ggammu.realspringbootjpa.repository.OrderSearch;
+import io.ggammu.realspringbootjpa.repository.order.simplequery.OrderSimpleQueryDto;
+import io.ggammu.realspringbootjpa.service.OrderService;
+import java.time.LocalDateTime;
+import java.util.List;
+import static java.util.stream.Collectors.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * xToOne(ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RequiredArgsConstructor
+@RestController
+public class OrderSimpleApiController {
+
+    private final OrderService orderService;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> orderV1() {
+        List<Order> all = orderService.findOrdersByString(new OrderSearch());
+        for (Order order: all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2() {
+        List<Order> orders= orderService.findOrdersByString(new OrderSearch());
+        List<SimpleOrderDto> simpleOrderDtos = orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(toList());
+        return simpleOrderDtos;
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> orderV3() {
+        List<Order> orders = orderService.findAllWithMemberDelivery();
+        List<SimpleOrderDto> simpleOrderDtos = orders.stream().map(SimpleOrderDto::new).collect(toList());
+        return simpleOrderDtos;
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> orderV4() {
+        return orderService.findAllOrderDto();
+    }
+
+    @Data
+    private class SimpleOrderDto {
+        private Long id;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            id = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+}
