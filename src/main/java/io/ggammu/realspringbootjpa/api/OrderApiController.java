4524d52bbceb72b61c9af241ed657dbf9f629de2
Young Hwang <ykcul02@gmail.com>
2021-12-30 23:30:09 +0900

    feat: dto를 이용한 OneToMany response

--- src/main/java/io/ggammu/realspringbootjpa/api/OrderApiController.java
+++ src/main/java/io/ggammu/realspringbootjpa/api/OrderApiController.java
@@ -1,10 +1,15 @@
 package io.ggammu.realspringbootjpa.api;
 
+import io.ggammu.realspringbootjpa.domain.Address;
 import io.ggammu.realspringbootjpa.domain.Order;
 import io.ggammu.realspringbootjpa.domain.OrderItem;
+import io.ggammu.realspringbootjpa.domain.OrderStatus;
 import io.ggammu.realspringbootjpa.repository.OrderRepository;
 import io.ggammu.realspringbootjpa.repository.OrderSearch;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +30,36 @@ public class OrderApiController {
             orderItems.stream().forEach(o -> o.getItem().getName());
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> orderv2() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> orderDtos = all.stream().map(OrderDto::new).collect(Collectors.toList());
+
+        return orderDtos;
+    }
+
+    @Getter
+    static class OrderDto {
+
+        private Long id;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItem> orderItems;
+
+        public OrderDto(Order order) {
+            this.id = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+            order.getOrderItems().stream().forEach(o -> o.getItem());
+            this.orderItems = order.getOrderItems();
+        }
+
     }
 
 }
